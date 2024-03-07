828b3f3e674405accde2a30c4ac6a54ee6a2b352
Lucas Danilo <lukastecno@gmail.com>
2024-03-07 17:51:23 -0300

    Search bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -2,20 +2,20 @@ import './styles.css';
 import Header from "../../components/Header";
 import SearchBar from '../../components/SearchBar';
 
-export default function Catalog(){
-    return(
+export default function Catalog() {
+    return (
         <>
-        <Header/>
-        <main className='ct-catalog-main'>
-            <section id="#ct-catalog-section">
-                <div className="ct-search-bar-container">
-                    <SearchBar />
-                </div>
-                <div className='ct-catalog-cards-container'>
-                    <div>Teste</div>
-                </div>
-            </section>
-        </main>
+            <Header />
+            <main className="ct-catalog-main">
+                <section id="#ct-catalog-section" className="ct-container">
+                    <div className="ct-search-bar-container">
+                        <SearchBar />
+                    </div>
+                    <div className="ct-catalog-cards-container">
+                        <div>Teste</div>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
